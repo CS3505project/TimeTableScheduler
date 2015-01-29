693389a663ef2a6171c94150ba2c1c9997b24873
jjor1 <jjor1@CS1-11-17.cs.ucc.ie>
2015-01-29 13:19:01 +0000

    git hub

---
+++ TimeTableScheduler/src/java/databasePackage/TestDB.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+    Tests a database installation by creating and querying
+    a sample table. Call this program as
+    java -classpath driver_class_path;. TestDB propertiesFile
+*/
+public class TestDB {
+    public static void main(String[] args) throws Exception {
+        SimpleDataSource.init("database.properties");
+
+        Connection conn = SimpleDataSource.getConnection();
+        try {
+            Statement stat = conn.createStatement();
+
+            ResultSet result = stat.executeQuery("SELECT * FROM test.User");
+            while (result.next()) {
+                System.out.println(result.getString("email"));
+            }
+        } finally {
+            conn.close();
+        }
+    }
+}
