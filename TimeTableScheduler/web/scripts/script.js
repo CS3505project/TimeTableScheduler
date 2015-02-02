d4324fe155e80eb065180fa07b5b2d5b70d617c8
lukasmirtschink <lukas.mirtschink@gmail.com>
2015-02-02 16:07:11 +0000

    Create script.js
    some un-elaborated-upon functions

---
+++ TimeTableScheduler/web/scripts/script.js
@@ -0,0 +1,8 @@
+function createHelp(){
+	//find context node to find out what page we need to display help about
+	var contextNode = document.getElementById("context");
+}
+
+function displayHelp(context){
+	// add the elements into the dom tree with the help for a given page
+}
