e9c7eb6de34a10f6cf7f9263f9157f0e73d5486a
kmon1 <112414248@umail.ucc.ie>
2015-02-03 13:54:47 +0000

    Update dbCreate.sql
    fixed small errors

--- dbCreate.sql
+++ dbCreate.sql
@@ -1,30 +1,30 @@
 CREATE TABLE `Cancellation` (
-  `modulecode` VARCHAR(255) NOT NULL,
+  `modulecode` VARCHAR(7) NOT NULL,
   `date` DATE NOT NULL,
   `description` LONGTEXT NOT NULL,
   PRIMARY KEY (`modulecode`, `date`)
 );
 
 CREATE TABLE `Practical` (
-  `modulecode` VARCHAR(255) NOT NULL,
-  `semester` VARCHAR(255) NOT NULL,
+  `modulecode` VARCHAR(7) NOT NULL,
+  `semester` VARCHAR(1) NOT NULL,
   `weekday` INTEGER NOT NULL,
   `time` INTEGER NOT NULL,
-  `room` VARCHAR(255) NOT NULL,
+  `room` VARCHAR(20) NOT NULL,
   `startdate` DATE NOT NULL,
-  `enddate` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`modulecode`, `weekday`)
+  `enddate` DATE NOT NULL,
+  PRIMARY KEY (`modulecode`, `semester`, `weekday`, `time`)
 );
 
 CREATE TABLE `PracticalCancellation` (
-  `modulecode` VARCHAR(255) NOT NULL,
+  `modulecode` VARCHAR(7) NOT NULL,
   `date` DATE NOT NULL,
   PRIMARY KEY (`modulecode`, `date`)
 );
 
 CREATE TABLE `TakesModule` (
   `uid` INTEGER NOT NULL,
-  `modulecode` VARCHAR(255) NOT NULL,
+  `modulecode` VARCHAR(7) NOT NULL,
   PRIMARY KEY (`uid`, `modulecode`)
 );
 
@@ -35,15 +35,15 @@ CREATE TABLE `Admin` (
 
 CREATE TABLE `Course` (
   `id` INTEGER PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL,
-  `department` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(20) NOT NULL,
+  `department` VARCHAR(50) NOT NULL,
   `year` INTEGER NOT NULL
 );
 
 CREATE TABLE `Group` (
   `groupid` INTEGER PRIMARY KEY,
-  `groupname` VARCHAR(255) NOT NULL,
-  `type` VARCHAR(255) NOT NULL
+  `groupname` VARCHAR(25) NOT NULL,
+  `type` VARCHAR(25) NOT NULL
 );
 
 CREATE TABLE `GroupTakesCourse` (
@@ -54,7 +54,7 @@ CREATE TABLE `GroupTakesCourse` (
 
 CREATE TABLE `GroupTakesModule` (
   `gid` INTEGER NOT NULL,
-  `modulecode` VARCHAR(255) NOT NULL,
+  `modulecode` VARCHAR(7) NOT NULL,
   PRIMARY KEY (`gid`, `modulecode`)
 );
 
@@ -71,11 +71,11 @@ CREATE TABLE `InGroup` (
 );
 
 CREATE TABLE `Lecture` (
-  `modulecode` VARCHAR(255) NOT NULL,
+  `modulecode` VARCHAR(7) NOT NULL,
   `semester` INTEGER NOT NULL,
   `weekday` INTEGER NOT NULL,
   `time` INTEGER NOT NULL,
-  `room` VARCHAR(255) NOT NULL,
+  `room` VARCHAR(20) NOT NULL,
   `startdate` DATE NOT NULL,
   `enddate` DATE NOT NULL,
   PRIMARY KEY (`modulecode`, `semester`, `weekday`, `time`)
@@ -84,34 +84,34 @@ CREATE TABLE `Lecture` (
 CREATE TABLE `Lecturer` (
   `uid` INTEGER PRIMARY KEY,
   `lecturerid` INTEGER NOT NULL,
-  `title` VARCHAR(255) NOT NULL
+  `title` VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE `Meeting` (
   `meetingid` INTEGER PRIMARY KEY,
   `date` DATE NOT NULL,
   `time` INTEGER NOT NULL,
-  `room` VARCHAR(255) NOT NULL,
+  `room` VARCHAR(20) NOT NULL,
   `description` LONGTEXT NOT NULL,
   `priority` INTEGER NOT NULL,
-  `organiser` VARCHAR(255) NOT NULL
+  `organiser` VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE `Module` (
-  `modulecode` VARCHAR(255) PRIMARY KEY,
+  `modulecode` VARCHAR(7) PRIMARY KEY,
   `description` LONGTEXT NOT NULL
 );
 
 CREATE TABLE `ModuleInCourse` (
-  `modulecode` VARCHAR(255) NOT NULL,
+  `modulecode` VARCHAR(7) NOT NULL,
   `courseid` INTEGER NOT NULL,
   PRIMARY KEY (`modulecode`, `courseid`)
 );
 
 CREATE TABLE `RoomChange` (
-  `modulecode` VARCHAR(255) NOT NULL,
+  `modulecode` VARCHAR(7) NOT NULL,
   `date` DATE NOT NULL,
-  `room` VARCHAR(255) NOT NULL,
+  `room` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`modulecode`, `date`)
 );
 
@@ -122,14 +122,14 @@ CREATE TABLE `Student` (
 
 CREATE TABLE `TeachesModule` (
   `lecturerid` INTEGER NOT NULL,
-  `modulecode` VARCHAR(255) NOT NULL,
+  `modulecode` VARCHAR(7) NOT NULL,
   PRIMARY KEY (`lecturerid`, `modulecode`)
 );
 
 CREATE TABLE `User` (
   `userid` INTEGER PRIMARY KEY AUTO_INCREMENT,
-  `passwordhash` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `firstname` VARCHAR(255) NOT NULL,
-  `surname` VARCHAR(255) NOT NULL
+  `passwordhash` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(70) NOT NULL,
+  `firstname` VARCHAR(50) NOT NULL,
+  `surname` VARCHAR(50) NOT NULL
 )
