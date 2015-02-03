fa2c9232e48c2f11de1dc450e103e94f6b54ff17
lukasmirtschink <>
2015-02-03 15:21:39 +0000

    skeleton for output class

---
+++ TimeTableScheduler/src/java/outputPackage/Output.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package outputPackage;
+
+/**
+ *
+ * @author lam1
+ */
+public class Output {
+    /**
+     * Creates the header which occurs in each page, with side bar etc.
+     * @return A string with all the HTML.
+     */
+    public String createHeader(){
+        String finalHTML = "";
+        //add file include from htmlIncludesfolder, conditional logic based on user type etc.
+        return finalHTML;
+    }
+    
+    /**
+     * Creates the footer which is on each page.
+     * @return A string with all the HTML.
+     */
+    public String createFooter(){
+        String finalHTML = "";
+        //add file include from htmlIncludesfolder
+        return finalHTML;
+    }
+}
