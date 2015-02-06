626bd499e710a22a0a4ee4086502c542f23d24c7
John O Riordan <>
2015-02-06 02:20:22 +0000

    Updated the timetable package

--- TimeTableScheduler/src/java/outputPackage/Output.java
+++ TimeTableScheduler/src/java/outputPackage/Output.java
@@ -32,12 +32,18 @@ public class Output {
      */
     public String createHeader() throws FileNotFoundException, IOException{
         String finalHTML = "";
-        //InputStream inStream = this.getClass().getClassLoader().getResourceAsStream("commonHeader.html");
-        //finalHTML += new Scanner(inStream).useDelimiter("\\Z").next();
+        InputStream inStream = this.getClass().getClassLoader().getResourceAsStream("commonHeader.html");
+        finalHTML += new Scanner(inStream).useDelimiter("\\Z").next();
         //add file include from htmlIncludesfolder, conditional logic based on user type etc.
-        TimeTable timetable = new TimeTable();
-        timetable.addDummyEvents();
-        finalHTML += timetable.createTimeTable(EventTime.EIGHT, EventTime.EIGHTTEEN, Day.MONDAY, Day.FRIDAY);
+        
+        /*
+         * Test need method to generate timetable HTML from actual event objects
+         * 
+         * TimeTable timetable = new TimeTable();
+         * timetable.addDummyEvents();
+         * finalHTML += timetable.createTimeTable(EventTime.EIGHT, EventTime.EIGHTTEEN, Day.MONDAY, Day.FRIDAY);
+         */
+        
         return finalHTML;
     }
     
