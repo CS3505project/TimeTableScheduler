9f19b46f172401420189a427dddfec2b50a36022
dol8 <dol8@CSG21-40.cs.ucc.ie>
2015-02-12 14:49:06 +0000

    did some code in groupPackage

--- TimeTableScheduler/src/java/groupPackage/ModuleGroup.java
+++ TimeTableScheduler/src/java/groupPackage/ModuleGroup.java
@@ -6,8 +6,22 @@ package groupPackage;
 
 /**
  *
- * @author jjor1
+ * @author jjor1 & donncha o leary
  */
-public class ModuleGroup {
+public class ModuleGroup extends Group{
+    
+    /**
+     * 
+     * @param groupId groupId group id from Group.java
+     * @param groupName group name from Group.java
+     * @param groupType group type from Group.java
+     */
+    public ModuleGroup(int groupId, String groupName, String groupType){
+        super(groupId,groupName,groupType);
+    }
+    
+    
+    
+    
     
 }
