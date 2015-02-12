f4dfb791f8382cfa37d3c74baea12267487545fa
dol8 <dol8@CSG21-40.cs.ucc.ie>
2015-02-12 15:23:49 +0000

    did some code in groupPackage

--- TimeTableScheduler/src/java/groupPackage/UserGroup.java
+++ TimeTableScheduler/src/java/groupPackage/UserGroup.java
@@ -4,20 +4,34 @@
  */
 package groupPackage;
 
+import java.util.ArrayList;
+import java.util.List;
+import userPackage.User;
+
+
 /**
  *
  * @author jjor1 & donncha o leary
  */
 public class UserGroup extends Group{
-    
+     private List<User> listOfUsers;
+     
+     
     /**
      * 
      * @param groupId group id from Group.java
      * @param groupName group name from Group.java
      * @param groupType group type from Group.java
      */
-    public UserGroup(int groupId, String groupName, String groupType){
+    public UserGroup(int groupId, String groupName, String groupType,List<User> listOfUsers){
         super(groupId,groupName,groupType);
+        this.listOfUsers = new ArrayList<User>();
     }
+    
+    // retrive list of users from database . 
+    // create user objects from each result returned from database and add them to list 
+    
+   
+    
     
 }
